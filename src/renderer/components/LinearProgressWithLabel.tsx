9a97422e7fb66586168cef97efd4dfa7dbbdc4ff
Michael Vastarelli <michael.vastarelli@gmail.com>
2022-03-04 19:20:56 -0500

    Printed value for linear progress bar is now embedded as a tooltip.

--- src/renderer/components/LinearProgressWithLabel.tsx
+++ src/renderer/components/LinearProgressWithLabel.tsx
@@ -1,24 +1,12 @@
-import PropTypes from 'prop-types';
-import { Typography, Box, LinearProgress } from '@mui/material';
+import { LinearProgress, Tooltip } from '@mui/material';
 import * as formatter from '../services/Formatters';
 
 export function LinearProgressWithLabel(props: { value: number }) {
   const { value } = props;
 
   return (
-    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-      <Box sx={{ width: '100%', mr: 1 }}>
-        <LinearProgress variant="determinate" value={value > 100 ? 100 : value} />
-      </Box>
-      <Box sx={{ minWidth: 35 }}>
-        <Typography variant="body2" color="text.secondary">
-          {formatter.percentage(value)}
-        </Typography>
-      </Box>
-    </Box>
+    <Tooltip title={formatter.percentage(value)}>
+      <LinearProgress variant="determinate" value={value > 100 ? 100 : value} />
+    </Tooltip>
   );
 }
-
-LinearProgressWithLabel.propTypes = {
-  value: PropTypes.number.isRequired,
-};
