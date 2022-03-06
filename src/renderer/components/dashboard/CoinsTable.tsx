e0601b45f928ce633ef978e26938ddac1c20a82e
Michael Vastarelli <michael.vastarelli@gmail.com>
2022-03-06 00:25:48 -0500

    Added external link to unmineable website so that the user can click on a coin in the coin table and view it within the website.

--- src/renderer/components/dashboard/CoinsTable.tsx
+++ src/renderer/components/dashboard/CoinsTable.tsx
@@ -1,11 +1,16 @@
-import { Table, TableContainer, TableCell, TableHead, TableRow, TableBody } from '@mui/material';
+import { Button, Table, TableContainer, TableCell, TableHead, TableRow, TableBody } from '@mui/material';
 import CheckIcon from '@mui/icons-material/Check';
 import { LinearProgressWithLabel } from '..';
 import * as formatter from '../../services/Formatters';
 import { ConfiguredCoin } from '../../../models';
+import { unmineableApi } from '../../../shared/UnmineableApi';
 
 function progress(mined: number | undefined, threshold: number | undefined) {
   return mined === undefined || threshold === undefined || mined === 0 || threshold === 0 ? 0 : (100 * mined) / threshold;
+}
+
+async function openBrowser(coin: string, address: string) {
+  await unmineableApi.openBrowser(coin, address);
 }
 
 export function CoinsTable(props: { coins: ConfiguredCoin[] }) {
@@ -34,7 +39,9 @@ export function CoinsTable(props: { coins: ConfiguredCoin[] }) {
               <TableCell>
                 <img src={c.icon} alt="icon" />
               </TableCell>
-              <TableCell>{c.symbol}</TableCell>
+              <TableCell>
+                <Button onClick={async () => openBrowser(c.symbol, c.address)}>{c.symbol}</Button>
+              </TableCell>
               <TableCell>{formatter.number(c.mined, 6)}</TableCell>
               <TableCell>{formatter.currency(c.price, 8)}</TableCell>
               <TableCell>{formatter.minedValue(c.price, c.mined)}</TableCell>
