ad29713bb1ee434087012e6e3729e49ac154a3b4
Michael Vastarelli <michael.vastarelli@gmail.com>
2022-03-12 18:42:16 -0500

    Added a throttle to the refresh observable to prevent the user from spamming outside networks.

--- src/models/Observables.ts
+++ src/models/Observables.ts
@@ -1,8 +1,10 @@
-import { BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
+import { BehaviorSubject, ReplaySubject, Subject, throttleTime } from 'rxjs';
 import { ConfiguredCoin } from './ConfiguredCoin';
 import { MinerState } from './MinerState';
+
+const REFRESH_THROTTLE = 1000 * 30;
 
 export const minerErrors$ = new Subject<string>();
 export const minerState$ = new BehaviorSubject<MinerState>({ state: 'inactive', currentCoin: null, miner: null });
 export const enabledCoins$ = new BehaviorSubject<ConfiguredCoin[]>([]);
-export const refreshData$ = new ReplaySubject(Date.now());
+export const refreshData$ = new ReplaySubject(Date.now()).pipe(throttleTime(REFRESH_THROTTLE));
