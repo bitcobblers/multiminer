4a1612579ca300550abb2fa0ea5739bda2d27e62
Michael Vastarelli <michael.vastarelli@gmail.com>
2022-03-23 18:29:44 -0400

    Added pause button to stop auto-scrolling the monitor output.

--- src/renderer/components/AutoScrollTextArea.tsx
+++ src/renderer/components/AutoScrollTextArea.tsx
@@ -2,23 +2,31 @@ import React, { useLayoutEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { useTheme } from '@mui/material/styles';
 
-type AutoScrollTextAreaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+type AutoScrollTextAreaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
+  isPaused?: boolean;
+};
 
 export function AutoScrollTextArea(props: AutoScrollTextAreaProps) {
-  const { value } = props;
+  const { value, isPaused } = props;
   const theme = useTheme();
   const textRef = useRef<HTMLTextAreaElement>(null);
 
+  const domProps = { ...props };
+  delete domProps.isPaused;
+
   useLayoutEffect(() => {
-    if (textRef.current !== null) {
-      textRef.current.scrollTop = textRef.current.scrollHeight;
+    if (!isPaused) {
+      if (textRef.current !== null) {
+        textRef.current.scrollTop = textRef.current.scrollHeight;
+      }
     }
-  }, [value]);
+  }, [isPaused, value]);
 
   // eslint-disable-next-line react/jsx-props-no-spreading
-  return <textarea {...props} ref={textRef} style={{ backgroundColor: theme.palette.background.paper, color: theme.palette.text.secondary, height: 'calc(100vh - 10rem)' }} />;
+  return <textarea {...domProps} ref={textRef} style={{ backgroundColor: theme.palette.background.paper, color: theme.palette.text.secondary, height: 'calc(100vh - 10rem)' }} />;
 }
 
 AutoScrollTextArea.propTypes = {
   value: PropTypes.string.isRequired,
+  isPaused: PropTypes.bool.isRequired,
 };
