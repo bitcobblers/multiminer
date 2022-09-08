5dd9336a477cbe4ed8b5d5f630841900190a0683
Michael Vastarelli <michael.vastarelli@gmail.com>
2022-09-07 23:55:17 -0400

    Removed direct dependency on notistack from appNotice$.

--- src/models/Observables.ts
+++ src/models/Observables.ts
@@ -1,9 +1,8 @@
-import { VariantType } from 'notistack';
 import { BehaviorSubject, Subject } from 'rxjs';
 import { ConfiguredCoin } from './ConfiguredCoin';
 import { MinerState } from './MinerState';
 
-export const appNotice$ = new Subject<{ variant: VariantType; message: string }>();
+export const appNotice$ = new Subject<{ variant: 'default' | 'error' | 'success' | 'warning' | 'info'; message: string }>();
 export const minerErrors$ = new Subject<string>();
 export const minerState$ = new BehaviorSubject<MinerState>({ state: 'inactive', currentCoin: null });
 export const enabledCoins$ = new BehaviorSubject<ConfiguredCoin[]>([]);
